4003fc1aa92445b473c4b0da9f187005d78dd888
Dan Gordon <dexter.urbane@gmail.com>
2013-07-11 11:57:41 +1000

    Initial commit.

---
+++ tests/tests.cc
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE test_template
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE (tests) // Name of test suite is test_template.
+
+BOOST_AUTO_TEST_CASE (test_sample)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
